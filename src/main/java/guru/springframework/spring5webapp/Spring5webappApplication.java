91f9da5bab53e00ca54b6afe16512b7af0cf722c
Cullen Sun <sunhongshuai@gmail.com>
2020-01-28 23:19:54 +0800

    Add publisher entity and repository

--- src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
+++ src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -1,11 +1,12 @@
 package guru.springframework.spring5webapp;
 
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Spring5webappApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Spring5webappApplication.class, args);
 	}
